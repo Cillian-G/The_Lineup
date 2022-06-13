904a6323c840b3532ba04b5086edf822160fe7ab
Cillian G <cilliangoan221@gmail.com>
2022-06-13 10:01:08 +0000

    Add Day and Beach models

--- session_scheduler/models.py
+++ session_scheduler/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+class Day(models.Model):
+    date = models.DateField
+    notes = models.TextField(default='No notes today, happy surfing! :)')
+    featured_image = CloudinaryField('image', default='placeholder')
+
+    class Meta:
+        ordering = ['date']
+
+
+class Beach(models.Model):
+    name = models.CharField(max_length=50)
+    facing = models.CharField(max_length=50)
+    latitude = models.CharField(max_length=50)
+    longitude = models.CharField(max_length=50)
+    notes = models.TextField()
+
+
+
