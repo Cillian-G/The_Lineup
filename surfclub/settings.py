f0d18f00ac9617cc774df6ce4627322da6667716
Cillian G <cilliangoan221@gmail.com>
2022-06-21 21:58:56 +0000

    Fix heroku deployment bug

--- surfclub/settings.py
+++ surfclub/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['surf-social.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['belmullet-lineup.herokuapp.com', 'localhost']
 
 
 # Application definition
